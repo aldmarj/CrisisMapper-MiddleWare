c33d6e57c429f7e7cf2ec32248b42076566d7d13
Aldmar <aldmar.joubert@students.plymouth.ac.uk>
2018-02-14 15:33:43 +0000

    Relationship between geo and tweet returned

--- MainAPI/api/controllers/neo4jCntroller.js
+++ MainAPI/api/controllers/neo4jCntroller.js
@@ -7,15 +7,25 @@ var driver =  neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '123456
 
 exports.list_all_tweets = function(req, res) {
     session
-        .run('MATCH(n:Tweet) RETURN n LIMIT 25')
+        .run('MATCH (tweet { tweetID: "913419275544748032" })-[:FROM]->(geo) RETURN tweet.tweetID,tweet.tweetText,tweet.tweetTime, geo.lat, geo.lon')
+        
         .then(function(result){
-            var movieArr = [];
+            var tweetArr = [];
             result.records.forEach(function(record){
-                console.log();
+                tweetArr.push({
+
+                    tweetID: record.get("tweet.tweetID"),
+                    tweetText: record.get("tweet.tweetText"),
+                    tweetTime: record.get("tweet.tweetTime"),
+                    tweetLat: record.get("geo.lat"),
+                    tweetLon: record.get("geo.lon")
+                    
+                });
+                console.log(record.get("tweet.tweetID"));
             });
-            res.send(movieArr);
+            res.send(tweetArr);
         })
         .catch(function(err){
-            console.log("err");
+            console.log(err);
         });
   };
