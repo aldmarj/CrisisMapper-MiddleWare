3b6fd1fb417ddf369327f6b5f388137f5286a96d
Aldmar <aldmar.joubert@students.plymouth.ac.uk>
2018-02-14 22:04:20 +0000

    Added CORS headers

--- MainAPI/api/routes/neo4jRoutes.js
+++ MainAPI/api/routes/neo4jRoutes.js
@@ -1,7 +1,11 @@
 'use strict';
 module.exports = function(app) {
   var api = require('../controllers/neo4jCntroller');
-
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
   // api Routes
   app.route('/tweets')
     .get(api.list_all_tweets)
