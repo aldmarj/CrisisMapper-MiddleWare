24757f4b0db26235218627c69045ca33f337f398
Aldmar <aldmar.joubert@students.plymouth.ac.uk>
2018-04-25 17:28:36 +0100

    connection to remote db made and pulling useful tweets

--- MainAPI/api/controllers/neo4jCntroller.js
+++ MainAPI/api/controllers/neo4jCntroller.js
@@ -2,26 +2,22 @@
 var neo4j = require('neo4j-driver').v1;
     
 
-var driver =  neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '123456')),
+var driver =  neo4j.driver('bolt://neo4j-core-0@35.187.165.149:7687', neo4j.auth.basic('neo4j', '')),
     session = driver.session();
 
 exports.list_all_tweets = function(req, res) {
     session
-        .run('MATCH (tweet { tweetID: "913419275544748032" })-[:FROM]->(geo) RETURN tweet.tweetID,tweet.tweetText,tweet.tweetTime, geo.lat, geo.lon')
+        .run('MATCH (a:Tweet) WHERE a.crisis = "Nepal Earthquake" RETURN a.text AS text, a.crisis AS crisis')
         
         .then(function(result){
             var tweetArr = [];
             result.records.forEach(function(record){
                 tweetArr.push({
 
-                    tweetID: record.get("tweet.tweetID"),
-                    tweetText: record.get("tweet.tweetText"),
-                    tweetTime: record.get("tweet.tweetTime"),
-                    tweetLat: record.get("geo.lat"),
-                    tweetLon: record.get("geo.lon")
+                    tweetText: record.get("text")
                     
                 });
-                console.log(record.get("tweet.tweetID"));
+                console.log(record.get("text"));
             });
             res.send(tweetArr);
         })
